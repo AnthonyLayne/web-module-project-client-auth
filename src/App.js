1484581c1936910178c6e4799058fb2498910605
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-26 14:29:22 -0400

    started setting up App and friends list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h2>MY FRIENDS</h2>
+          <ul>
+            <li>
+              <Link className="link" to="login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="friends">
+                Friends List
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="friends/add">
+                Add Friends
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/logout">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </header>
+
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const FriendsList = () => {
+  const [friends, setFriends] = useState();
+
+  return (
+    <div>
+      <h2>My Friends</h2>
+    </div>
+  );
+};
