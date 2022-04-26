1cd9eb1ae06dbdfe56e7fa959de54509bcbf3399
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-26 13:53:32 -0400

    setting up the login page handlechange

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const Login = () => {
+  //need state, handlechange and handlesubmit
+  const [credentials, setCredentials] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      username: "Bloom",
+      password: "Tech",
+    });
+  };
+
   return (
     <div>
       <h1>Login</h1>
       <form>
         <label>UserName</label>
-        <input />
+        <input onChange={handleChange} name="username" id="username" />
         <label>Password</label>
-        <input />
+        <input onChange={handleChange} name="password" id="password" />
       </form>
     </div>
   );
