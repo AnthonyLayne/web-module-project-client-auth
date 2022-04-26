16671e3941c28a76d6f86ca3beb9eadd0373bd2e
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-26 13:46:51 -0400

    setting up the login page

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>UserName</label>
+        <input />
+        <label>Password</label>
+        <input />
+      </form>
+    </div>
+  );
+};
