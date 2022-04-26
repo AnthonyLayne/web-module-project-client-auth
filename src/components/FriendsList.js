9f413d308da53c6e811ad2adacf1991f34f75691
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-26 14:44:30 -0400

    setup friends list useeffect

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const FriendsList = () => {
   const [friends, setFriends] = useState();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+
+    axios
+      .get("http://localhost:9000/api/friends", {
+        headers: {
+          authorization: token,
+        },
+      })
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
